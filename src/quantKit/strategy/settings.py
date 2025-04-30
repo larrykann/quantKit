66a2cc2127b0645467452783e0a1a905ccb6d6dd
Larry Kann <larry@huntgathertrade.com>
2025-04-30 13:42:06 -0400

    adding validation period to settings for is/oos

--- src/quantKit/strategy/settings.py
+++ src/quantKit/strategy/settings.py
@@ -6,6 +6,7 @@ class StrategySettings:
     start_date: Optional[str] = None
     end_date: Optional[str] = None
     intraday_start_date: Optional[str] = None
+    validation_days: Optional[int] = None
 
     def __post_init__(self):
         # intraday defaults to start_date if not specified
@@ -16,5 +17,6 @@ class StrategySettings:
 DEFAULT_STRATEGY_SETTINGS = StrategySettings(
     start_date=None,
     end_date=None,
-    intraday_start_date=None
+    intraday_start_date=None,
+    validation_days=None
 )
